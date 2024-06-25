829dd2a564ab70f62a68649ed7163fe8e4783b36
Amit854459 <amitkumarit764@gmail.com>
2024-06-24 23:07:41 -1200

    Initial version

---
+++ src/main/java/com/example/banking_app/controller/AccountController.java
@@ -0,0 +1,75 @@
+package com.example.banking_app.controller;
+
+import com.example.banking_app.dto.AccountDto;
+import com.example.banking_app.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //todo add account REST API
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+    //todo get account REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto>  getAccountById(@PathVariable Long id){
+        AccountDto accountDto= accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+
+    // todo Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto>  deposit(@PathVariable Long id,
+                                               @RequestBody Map<String,Double> request){
+        Double amount = request.get("amount");
+        AccountDto accountDto=accountService.deposit(id, amount);
+        return  ResponseEntity.ok(accountDto);
+
+    }
+
+    //todo  withdraw  REST API
+    @PutMapping("/{id}/withdraw")
+    public  ResponseEntity<AccountDto>  withdraw(@PathVariable Long id,
+                                                @RequestBody Map<String,Double> request){
+        double amount = request.get("amount");
+        AccountDto accountDto= accountService.withdraw(id,amount);
+        return  ResponseEntity.ok(accountDto);
+
+    }
+
+
+    //todo Get All Accounts REST API
+    @GetMapping
+    public  ResponseEntity<List<AccountDto>>  getAllAccounts(){
+        List<AccountDto> accounts=accountService.getAllAccounts();
+
+        return  ResponseEntity.ok(accounts);
+    }
+
+    //TODO  DELETE Account REST API
+    @DeleteMapping("/{id}")
+    public ResponseEntity< String> deleteAccount(@PathVariable Long id){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok(" Account is deleted  successfully ! ");
+}
+
+
+
+
+}
---
+++ src/main/java/com/example/banking_app/service/AccountService.java
@@ -0,0 +1,17 @@
+package com.example.banking_app.service;
+
+import com.example.banking_app.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+    AccountDto  getAccountById(Long id);
+    AccountDto deposit(Long id,double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
+    void deleteAccount(Long id );
+}
